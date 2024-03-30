2f9fb57f2c7c94011c123efcb5bd7a625fa8f546
findreamer <findreame@126.com>
2024-03-30 15:32:25 +0800

    feat: init mysql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,42 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import {Connection} from 'typeorm'
 
+const getDatabaseConfig = () => {
+  const configService = new ConfigService()
+  const config: TypeOrmModuleOptions = {
+    type: 'mysql',
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_DATABASE'),
+    autoLoadEntities: true
+
+  }
+  console.log('get config   => ', config)
+  return TypeOrmModule.forRoot(config)
+}
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    getDatabaseConfig()
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+
+
+export class AppModule {
+
+  constructor(private readonly connection: Connection) {
+    this.connection.connect().then(() => {
+      console.log('success')
+    }).catch(err => {
+      console.log('fail')
+    })
+  }
+ }
