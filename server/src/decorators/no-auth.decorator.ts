4146661be399ddf3b4c82761cf4bf5bf064583be
findreamer <findreame@126.com>
2024-05-14 21:41:47 +0800

    feat: 增加noAuth装饰器

--- server/src/decorators/no-auth.decorator.ts
+++ server/src/decorators/no-auth.decorator.ts
@@ -1,2 +1,3 @@
-import { SetMetadata} from '@nestjs/common'
+import { SetMetadata } from '@nestjs/common'
+//  SetMetadata ，它会为被这个装饰器修饰的类或方法加上一个 noAuth 属性
 export const NoAuth = () => SetMetadata('noAuth', true)
