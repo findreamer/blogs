b7a81341aedff6786180ca10f511f87e3b23f780
findreamer <findreame@126.com>
2024-05-28 21:04:57 +0800

    feat: 初步完成编辑器页面配置

--- frontend/src/views/Editor/index.tsx
+++ frontend/src/views/Editor/index.tsx
@@ -1,11 +1,32 @@
 import { useState } from "react";
-import { Button, Popover, Row, Input, Form, Select } from "antd";
+import { Button, Popover, Row, Input, Form, Select, DatePicker } from "antd";
+import { Editor as BEditor } from "@bytemd/react";
+import { uploadFile } from "@/api/user";
+import dayjs from "dayjs";
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+import "bytemd/dist/index.css";
+import zh from "bytemd/locales/zh_Hans.json";
+import "highlight.js/styles/default.css";
+import "./editor.css";
+const plugins = [gfm(), highlight()];
 
 const Editor = () => {
   const [title, setTitle] = useState("");
   const [form] = Form.useForm();
   const [categoryList, setCategoryList] = useState([]);
   const [data, setData] = useState<any>({});
+  const [value, setValue] = useState("");
+
+  const publish = () => {};
+  const handleUpload = async (files: File[]) => {
+    const formData = new FormData();
+    for (const file of files) {
+      formData.append("files", file);
+    }
+    const res = await uploadFile(formData);
+    return res.data.map((item: any) => ({ url: item.url }));
+  };
 
   return (
     <div className="h-full flex flex-col">
@@ -34,6 +55,47 @@ const Editor = () => {
                 >
                   <Select options={categoryList} />
                 </Form.Item>
+
+                <Form.Item
+                  rules={[{ required: true, message: "请输入简介" }]}
+                  name="introduction"
+                  label="简介"
+                >
+                  <Input.TextArea maxLength={100} showCount />
+                </Form.Item>
+
+                {data.status === 0 && (
+                  <Form.Item name="time" label="定时发布">
+                    <DatePicker
+                      disabledTime={() => {
+                        const range = (start: number, end: number) => {
+                          const result = [];
+                          for (let i = start; i < end; i++) {
+                            result.push(i);
+                          }
+                          return result;
+                        };
+                        const now = new Date();
+                        const hour = now.getHours();
+                        const minute = now.getMinutes();
+                        return {
+                          disabledHours: () =>
+                            range(0, 24).filter((h) => h < hour),
+                          disabledMinutes: () =>
+                            range(0, 60).filter((m) => m - 5 < minute),
+                        };
+                      }}
+                      disabledDate={(current) => {
+                        return current && current < dayjs().startOf("day");
+                      }}
+                      showTime={{ format: "HH:mm" }}
+                      format="YYYY-MM-DD HH:mm"
+                    />
+                  </Form.Item>
+                )}
+                <Button onClick={publish} type="primary">
+                  发布
+                </Button>
               </Form>
             </div>
           }
@@ -42,7 +104,20 @@ const Editor = () => {
         </Popover>
       </Row>
 
-      <div></div>
+      <div className="flex flex-1 flex-col markdown-wrapper">
+        <div className="px-4 overflow-y-auto beditor h-full">
+          <BEditor
+            uploadImages={handleUpload}
+            mode="split"
+            locale={zh}
+            value={value}
+            plugins={plugins}
+            onChange={(v) => {
+              setValue(v);
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
